bac9778864b27a5788156482381bc1660837b5e1
S-Syrichko <s-syrichko@protonmail.com>
2023-03-28 18:41:47 +0200

    Fourth deploy on GTPages

---
+++ static/js/677.aa71f25a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkasa_fr=self.webpackChunkkasa_fr||[]).push([[677],{677:function(r,t,e){e.r(t),e.d(t,{default:function(){return a}});e(791);var c=e(87),s="Thumb_root__ZVkEd",n=e(184),a=function(r){var t=r.item;return(0,n.jsx)(c.rU,{to:"/product/".concat(t.id),children:(0,n.jsxs)("div",{className:s,children:[(0,n.jsx)("img",{src:t.cover,alt:t.title}),(0,n.jsx)("h2",{children:t.title})]})})}}}]);
+//# sourceMappingURL=677.aa71f25a.chunk.js.map
